39db1334e9e18e71327791a220498ce1a6654436
corey <corey.cushnie@gmail.com>
2024-04-22 10:18:52 -0400

    top down build

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Subscribe } from './components';
-import { SafeAreaView } from 'react-native-web';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  SafeAreaView,
+  KeyboardAvoidingView,
+} from "react-native";
+import { Card, Profile, Subscribe } from "./components";
+
+const LINKS = [
+  {
+    name: "LinkedIn",
+    description: "Link to my linkedin account.",
+    link: "https://linkedin.com/in/corey-cushnie",
+  },
+  {
+    name: "Web Portfolio",
+    description: "My personal web portfolio.",
+    link: "https://coreycushnie.com",
+  },
+];
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.main}>
-        <Text></Text>
-
-        <Subscribe/>
-        <StatusBar style="auto" />
-      </View>
+      <KeyboardAvoidingView style={{ height: "100%" }} behavior="padding">
+        <View style={styles.main}>
+          <Profile />
+          <ScrollView style={styles.linkContainer}>
+            {LINKS.map((values, index) => {
+              return <Card key={index} {...values} />;
+            })}
+          </ScrollView>
+          <Subscribe />
+          <StatusBar style="auto" />
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -19,16 +43,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'black',
 
-    fontFamily: "Inter",
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    backgroundColor: "black",
   },
-  main:{
-    width: '100%',
-    padding: 20
-  }
+  main: {
+    width: "100%",
+    height: "100%",
+    padding: 20,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    rowGap: 10,
+    color: "black",
+  },
+  linkContainer: {
+    width: "100%",
+    flex: 1,
+    display: "flex",
+    rowGap: 10,
+  },
 });
