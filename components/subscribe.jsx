46650fee4f36f495e1d914ae7718c378ddf674e6
corey <corey.cushnie@gmail.com>
2024-04-22 10:19:44 -0400

    update logic

--- components/subscribe.jsx
+++ components/subscribe.jsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { StyleSheet, Pressable, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Icon } from "react-native-elements";
 import { Btn } from "./button";
 
-export const Subscribe = ({ name, image }) => {
+export const Subscribe = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Signup to my newsletter</Text>
@@ -10,9 +13,15 @@ export const Subscribe = ({ name, image }) => {
         style={styles.input}
         placeholder="Your email address here"
         placeholderTextColor="#555"
-        keyboardType="emal"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
       />
-      <Btn name="Subscribe" />
+      <Btn
+        name="Subscribe"
+        icon={<Icon name="rss" type="feather" />}
+        onPress={email}
+      />
     </View>
   );
 };
@@ -21,24 +30,24 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#111",
     padding: 20,
-    border: "1px solid #333",
+    borderWidth: 1,
+    borderColor: "#333",
     borderRadius: 10,
     width: "100%",
   },
   text: {
-    flex: 1,
     color: "white",
-    fontSize: "18px",
+    fontSize: 18,
     marginBottom: 10,
   },
   input: {
-    paddingVertical: 10,
+    paddingVertical: 15,
     marginVertical: 10,
     color: "white",
     backgroundColor: "#000",
-    border: "1px solid #222",
+    borderWidth: 1,
+    borderColor: "#222",
     paddingHorizontal: 10,
     borderRadius: 5,
-    outlineStyle: "none",
   },
 });
