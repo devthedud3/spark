83bae8e36e1105590b0c1bcd89c5817b7c2405f0
corey <corey.cushnie@gmail.com>
2024-04-22 14:01:04 -0400

    init data

---
+++ data.js
@@ -0,0 +1,28 @@
+export const profile =
+  "Product/Platform Engineer with a diverse skill\
+ set encompassing Web Development, Data Processing,\
+ and Application Development. With over 5 years of hands-on\
+ experience, I excel in building software and analyzing data\
+ to drive business success. I specialize in developing models\
+ and reactive websites with a strong emphasis on UX/UI design,\
+ utilizing React and Node to deliver cutting-edge solutions.\
+ Committed to writing clean and efficient code, I strive to create\
+ user-friendly applications that meet the highest standards.";
+
+export const LINKS = [
+  {
+    name: "LinkedIn",
+    description: "Link to my linkedin account.",
+    link: "https://linkedin.com/in/corey-cushnie",
+  },
+  {
+    name: "Web Portfolio",
+    description: "My personal web portfolio.",
+    link: "https://coreycushnie.com",
+  },
+  {
+    name: "LXD",
+    description: "(In Development) - E-Commerce website CRWN V2",
+    link: "https://lxd-xi.vercel.app/",
+  },
+];
